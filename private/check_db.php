ed578f42c318a17ea32d9a47a486ab2c21b41645
Nebula [NMV] <nebula.nmv@gmail.com>
2025-05-13 12:38:30 +0700

    Update check_db.php

--- private/check_db.php
+++ private/check_db.php
@@ -1,23 +1,12 @@
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "library2";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "library2";
 
-// $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-$host = "sql107.infinityfree.com"; // Check if this is correct
-$username = "if0_38938772";
-$password = "u1mDknqqzHqxcA";
-$database = "if0_38938772_library2";
 
-$conn = new mysqli($host, $username, $password, $database);
-
-if ($conn->connect_error) {
-    echo "ERROR";
-} else {
-    echo "OK";
-}
 
 
 
